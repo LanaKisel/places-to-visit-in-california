88a31ddb4aa954136b9a7a32300334557a0fc064
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-12-27 13:31:56 -0800

    adds About page

--- src/About.js
+++ src/About.js
@@ -6,6 +6,7 @@ const About = () => {
         <h1>Visit California</h1>
         <hr/>
         <br/>
+        <br/>
         <p>Visit California. Our mission is to inspire people to explore this beautiful world. Why California? It's our homeland, which makes it a perfect landmark to start our adventures. We're trying to popularize the idea of traveling that doesn't require fancy hotels, expensive flight tickets or weeks of time of. We want to make exploring beautiful places more commono by raising the awareness of the places that so close in distance to many people. We hope we can inspire people to spend their weekends in nature, fall in love with exploring and fiding unforgettable adventures on the way.</p>   
     </div>
   )
