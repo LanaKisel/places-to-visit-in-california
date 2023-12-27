307edf9073a07483fc19c29887c0947fd218fc8e
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-12-27 13:40:47 -0800

    fetch POST

--- src/PlaceForm.js
+++ src/PlaceForm.js
@@ -7,18 +7,26 @@ const PlaceForm = () => {
         image: ""
     })
 
-    useEffect(()=>{
-        fetch("http://localhost:3000/places",)
-    },[])
-
-
     const handleChange=(e)=>{
-        setPLace({...place, [e.target.title]:e.target.value})
+        setPLace({...place, [e.target.name]:e.target.value})
     }
 
     const handleSubmit=(e)=>{
-        e.preventDefault()
-
+        e.preventDefault();
+        const newPlace={
+        title: place.title,
+        description: place.description,
+        image: place.image
+        }
+        fetch("http://localhost:3000/places", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newPlace) 
+    })
+    .then(r=>r.json())
+    .then(data=>console.log(data))
     }
   return (
     <div>
