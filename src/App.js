619df1c3cb73b2a3ebc87aa34f71eb2472b2b866
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-12-26 19:48:57 -0800

    added  new elements and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      Hi Lana!
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/places" element={<Places/>}></Route>
+          <Route path="/places/new" element={<PlaceForm/>}></Route>
+          <Route path="/places/:id" element={<Place/>}></Route>
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
