32b872af74af90ce3d0e1f6a5dd93a35e4d5a618
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-12-27 17:27:58 -0800

    all changes so far

--- src/PlaceForm.js
+++ src/PlaceForm.js
@@ -1,47 +1,50 @@
-import React, {useEffect, useState, useCallback} from 'react'
+import React, { useState, useCallback } from 'react'
 
-const PlaceForm = () => {
-    const [place, setPlace]=useState({
+const PlaceForm = (props) => {
+    const [place, setPlace] = useState({
         title: "",
         description: "",
         image: ""
     })
 
-    const handleChange=(e)=>{
-        setPlace({...place, [e.target.name]:e.target.value})
+    const handleChange = (e) => {
+        setPlace({ ...place, [e.target.name]: e.target.value })
     }
 
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        const newPlace={
-        title: place.title,
-        description: place.description,
-        image: place.image
+        const newPlace = {
+            title: place.title,
+            description: place.description,
+            image: place.image
         }
         fetch("http://localhost:3000/places", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newPlace) 
-    })
-    .then(r=>r.json())
-    .then(data=>setPlace(data))
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newPlace)
+        })
+            .then(r => r.json())
+            .then(data => {
+                console.log(props, props.history)
+               props.history.push('/places')
+            })
     }
-  return (
-    <div>
-        <form onSubmit={handleSubmit}>
-            <label>Title</label>
-            <input name="title" onChange={handleChange} type="text"/><br/>
-            <label>Description</label>
-            <input name="description" onChange={handleChange} type="text"/><br/>
-            <label>Image</label>
-            <input name="image" onChange={handleChange}  type="text"/><br/>
-            <input type="submit"/>
-        </form>
-      
-    </div>
-  )
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input name="title" onChange={handleChange} type="text" /><br />
+                <label>Description</label>
+                <input name="description" onChange={handleChange} type="text" /><br />
+                <label>Image</label>
+                <input name="image" onChange={handleChange} type="text" /><br />
+                <input type="submit" />
+            </form>
+
+        </div>
+    )
 }
 
 export default PlaceForm
