e12a4ccbd426991ade266a6e437c8a46069bc806
Lana Kisel <svetlana.kisel.ca@gmail.com>
2024-01-06 11:13:01 -0800

    renders photos in gallery format

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react'
 import { PlacesContext } from './Context.js';
-import Image from './Image.js';
+import Gallery from 'react-photo-gallery';
 
 const Home = () => {
   const { places, setPlaces } = useContext(PlacesContext)
@@ -9,13 +9,19 @@ const Home = () => {
         .then(r => r.json())
         .then(data => setPlaces(data))
 }
-  const newImage=places.map(place=><Image key={place.id} alt={place.title} image={place.image}/>)
+  const photos = places.map(p=>{
+    return {
+      src: p.image,
+      width: 1,
+      height: 1
+    }
+  })
   return (
     <div>
       <h1>Welcome to  California</h1>
       <hr/>
       <p style={{textAlign:'center', marginLeft: 40 , marginRight: 40 } }>So, you've found yourself in the state of California and want to experience the best of it? Or maybe you are in love with traveling and found some cool hidden places and can't wait to share it with others? Congratulations, you've come to the right place! This Page will provide you with information about amazing places, that you can explore. It also allows you to brag about hidden jams you've visited. Have fun!</p>
-      {newImage}
+      <Gallery photos={photos} />;
     </div>
   )
 }
